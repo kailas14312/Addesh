a770f91279eba37eae268b29115a608ea1df9fb8
Kailas Dahiwal <mamtadahiwal@gmail.com>
2024-05-27 20:50:00 +0530

    added

---
+++ Addesh/src/Code.java
@@ -0,0 +1,20 @@
+
+
+public class Code {
+	
+	public static void main(String[] args) {
+		
+		int fact =1;
+		int no=5;
+		
+		for(int i=fact; i <=no; i++) {
+			
+			fact=fact*i;
+			
+			System.out.println(fact);
+		}
+		
+		
+	}
+
+}
